b978156bfd5991bce700728e580317d22feedf08
mafcsst-svg <mafcsst@gmail.com>
2026-02-08 09:58:35 -0300

    Update firebaseConfig.ts

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,13 +1,14 @@
-// import { initializeApp } from "firebase/app";
+// 1. Uncomment the import
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyALw3abSubpKKJGmPql0PJdQc61UaCa654",
   authDomain: "hortal-delivery.firebaseapp.com",
   projectId: "hortal-delivery",
-  storageBucket: "hortal-delivery.firebasestorage.app",
+  storageBucket: "hortal-delivery.appspot.com",
   messagingSenderId: "560015064869",
   appId: "1:560015064869:web:d39ac269d33c2e8119a4cc"
 };
 
-// export const app = initializeApp(firebaseConfig);
-export const app = null;
+// 2. Uncomment this line and REMOVE the "export const app = null;"
+export const app = initializeApp(firebaseConfig);
