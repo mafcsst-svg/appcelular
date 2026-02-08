a56a2a80b2b40098d9b04d9ea784f9b136f04c29
mafcsst-svg <mafcsst@gmail.com>
2026-02-08 10:13:55 -0300

    Update firebaseConfig.ts

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -4,10 +4,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyALw3abSubpKKJGmPql0PJdQc61UaCa654",
   authDomain: "hortal-delivery.firebaseapp.com",
   projectId: "hortal-delivery",
-  storageBucket: "hortal-delivery.firebaseapp.com", // Fixed a typo here too
+  storageBucket: "hortal-delivery.firebasestorage.app",
   messagingSenderId: "560015064869",
   appId: "1:560015064869:web:d39ac269d33c2e8119a4cc"
 };
 
-// This line MUST be active (uncommented)
+// This line initializes the app and exports it for the rest of your project
 export const app = initializeApp(firebaseConfig);
